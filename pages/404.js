3f4d625ca8db0c24576075eb52a184be213841c3
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2023-04-29 02:37:53 -0300

    404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+        <div style={{
+            display: "flex",
+        }}>
+            <h2>404 | This page could not be found</h2>
+        </div>
+    );
+}
+
+export default NotFound;
